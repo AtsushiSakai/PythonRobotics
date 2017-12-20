a7fec910482b928df964d8af66a39ea479d8e768
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-12-19 22:04:01 -0800

    add reedsshepppath test

--- PathPlanning/ReedsSheppPath/reeds_shepp_path_planning.py
+++ PathPlanning/ReedsSheppPath/reeds_shepp_path_planning.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python
-# -*- coding: utf-8 -*-
 """
 
 Reeds Shepp path planner sample code
---
+++ tests/test_reeds_shepp_path_planning.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+#  from PathPlanning.ReedsSheppPath import reeds_shepp_path_planning
+import math
+
+
+class Test(TestCase):
+
+    def test1(self):
+        pass
