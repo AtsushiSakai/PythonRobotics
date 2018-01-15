c8a63427f198176c591fa6bbb4ceb805cd92dee2
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-01-14 16:24:48 -0800

    fix test

--- PathPlanning/StateLatticePlanner/state_lattice_planner.py
+++ PathPlanning/StateLatticePlanner/state_lattice_planner.py
@@ -230,7 +230,7 @@ def uniform_terminal_state_sampling_test2():
 
 
 def biased_terminal_state_sampling_test1():
-    lane_state_sampling_test1 = 0.0
+    k0 = 0.0
     nxy = 30
     nh = 2
     d = 20
