cfd31a9cb7e66c59094929258f729696b0d0fcf3
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-01-19 13:58:09 -0800

    add model_predictive_speed_and_steer_control

---
+++ PathTracking/model_predictive_speed_and_steer_control/model_predictive_speed_and_steer_control.py
@@ -0,0 +1,15 @@
+"""
+
+Path tracking simulation with model predictive control for speed and steer control
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+
+def main():
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()
