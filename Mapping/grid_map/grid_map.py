3952b9ceef7945a23e47ad1e5184f2b80b9594aa
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-02-12 22:33:10 -0800

    add figure

--- Mapping/grid_map/grid_map.py
+++ Mapping/grid_map/grid_map.py
@@ -6,9 +6,48 @@ author: Atsushi Sakai (@Atsushi_twi)
 
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+AREA_WIDTH = 30.0
+
+
+def generate_ray_casting_grid_map(ox, oy, reso):
+
+    minx = min(ox) - AREA_WIDTH / 2.0
+    miny = min(oy) - AREA_WIDTH / 2.0
+    maxx = max(ox) + AREA_WIDTH / 2.0
+    maxy = max(oy) + AREA_WIDTH / 2.0
+    xw = round((maxx - minx) / reso)
+    yw = round((maxy - miny) / reso)
+
+    # calc each potential
+    pmap = [[0.0 for i in range(yw)] for i in range(xw)]
+
+    for ix in range(xw):
+        x = ix * reso + minx
+
+        for iy in range(yw):
+            y = iy * reso + miny
+            pmap[ix][iy] = x + y
+
+    draw_heatmap(pmap)
+    plt.show()
+
+
+def draw_heatmap(data):
+    data = np.array(data).T
+    plt.pcolor(data, vmax=100.0, cmap=plt.cm.Blues)
+
 
 def main():
     print(__file__ + " start!!")
+
+    ox = [0.0]
+    oy = [0.0]
+    reso = 1.0
+
+    generate_ray_casting_grid_map(ox, oy, reso)
 
 
 if __name__ == '__main__':
