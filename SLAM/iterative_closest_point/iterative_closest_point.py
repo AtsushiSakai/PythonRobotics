273728acff987d8980654f4c258cf5cbc61609b0
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-02-20 09:36:42 -0800

    start implementation of ICP

---
+++ SLAM/iterative_closest_point/iterative_closest_point.py
@@ -0,0 +1,19 @@
+"""
+
+Iterative Closet Point (ICP) SLAM example
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+#  import math
+#  import numpy as np
+#  import matplotlib.pyplot as plt
+
+
+def main():
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()
