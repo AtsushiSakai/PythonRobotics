3e9005d92270160a269f5203515e64c66a8cf6bb
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-02-23 21:02:19 -0800

    add ekfslam code

---
+++ SLAM/EKFSLAM/ekf_slam.py
@@ -0,0 +1,15 @@
+"""
+
+Extended Kalman Filter based SLAM example
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+
+def main():
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()
