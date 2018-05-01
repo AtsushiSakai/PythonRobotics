74f61eda18fbc2230cb7079c58aa1f990f30c99c
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-05-01 16:50:20 +0900

    start kmean_clustering sample

---
+++ Mapping/kmean_clustering/kmean_clustering.py
@@ -0,0 +1,16 @@
+"""
+
+Object clustering with k-mean algorithm
+
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+
+def main():
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()
