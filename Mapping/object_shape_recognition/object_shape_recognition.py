36a5d4fb862ca3098e83ad68fcf3fcb02af01392
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-05-12 10:54:10 +0900

    start object_shape_recognition code implementation

---
+++ Mapping/object_shape_recognition/object_shape_recognition.py
@@ -0,0 +1,15 @@
+"""
+
+Object shape recognition with convex optimization
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+
+def main():
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()
