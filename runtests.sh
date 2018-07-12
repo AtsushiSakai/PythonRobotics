f34e7201bbe4d3f6201adfbce590f06e1a814196
Toku1105 <fa_ing.toku@hotmail.com>
2018-07-12 16:36:22 -0500

    Java Implementation w/ Matrix mini library

--- runtests.sh
+++
@@ -1,2 +0,0 @@
-echo "Run test suites! "
-python -m unittest discover tests 
--- tests/test_probabilistic_road_map.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from PathPlanning.ProbabilisticRoadMap import probabilistic_road_map
-
-
-class Test(TestCase):
-
-    def test1(self):
-        probabilistic_road_map.show_animation = False
-        probabilistic_road_map.main()
