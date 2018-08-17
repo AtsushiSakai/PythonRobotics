74f6fc1a52142bde096d7b6aaca4bdbb2fbc7c25
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-08-17 21:29:47 +0900

    add animation

--- ArmNavigation/n_joint_to_point_control/n_joint_to_point_control.py
+++ ArmNavigation/n_joint_to_point_control/n_joint_to_point_control.py
@@ -2,31 +2,35 @@
 Inverse kinematics for an n-link arm using the Jacobian inverse method
 
 Author: Daniel Ingram (daniel-s-ingram)
+        Atsushi Sakai (@Atsushi_twi)
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
 from NLinkArm import NLinkArm
 
-#Simulation parameters
+# Simulation parameters
 Kp = 2
 dt = 0.1
 N_LINKS = 10
 N_ITERATIONS = 10000
 
-#States
+# States
 WAIT_FOR_NEW_GOAL = 1
 MOVING_TO_GOAL = 2
 
-def n_link_arm_ik():
+show_animation = True
+
+
+def main():
     """
     Creates an arm using the NLinkArm class and uses its inverse kinematics
     to move it to the desired position.
     """
-    link_lengths = [1]*N_LINKS
-    joint_angles = np.array([0]*N_LINKS)
+    link_lengths = [1] * N_LINKS
+    joint_angles = np.array([0] * N_LINKS)
     goal_pos = [N_LINKS, 0]
-    arm = NLinkArm(link_lengths, joint_angles, goal_pos)
+    arm = NLinkArm(link_lengths, joint_angles, goal_pos, show_animation)
     state = WAIT_FOR_NEW_GOAL
     solution_found = False
     while True:
@@ -35,10 +39,11 @@ def n_link_arm_ik():
         end_effector = arm.end_effector
         errors, distance = distance_to_goal(end_effector, goal_pos)
 
-        #State machine to allow changing of goal before current goal has been reached
+        # State machine to allow changing of goal before current goal has been reached
         if state is WAIT_FOR_NEW_GOAL:
             if distance > 0.1 and not solution_found:
-                joint_goal_angles, solution_found = inverse_kinematics(link_lengths, joint_angles, goal_pos)
+                joint_goal_angles, solution_found = inverse_kinematics(
+                    link_lengths, joint_angles, goal_pos)
                 if not solution_found:
                     print("Solution could not be found.")
                     state = WAIT_FOR_NEW_GOAL
@@ -47,13 +52,14 @@ def n_link_arm_ik():
                     state = MOVING_TO_GOAL
         elif state is MOVING_TO_GOAL:
             if distance > 0.1 and (old_goal is goal_pos):
-                joint_angles = joint_angles + Kp*ang_diff(joint_goal_angles, joint_angles)*dt
+                joint_angles = joint_angles + Kp * \
+                    ang_diff(joint_goal_angles, joint_angles) * dt
             else:
                 state = WAIT_FOR_NEW_GOAL
                 solution_found = False
 
         arm.update_joints(joint_angles)
-            
+
 
 def inverse_kinematics(link_lengths, joint_angles, goal_pos):
     """
@@ -69,12 +75,64 @@ def inverse_kinematics(link_lengths, joint_angles, goal_pos):
         joint_angles = joint_angles + np.matmul(J, errors)
     return joint_angles, False
 
+
+def get_random_goal():
+    from random import random
+    SAREA = 15.0
+    return [SAREA * random() - SAREA / 2.0,
+            SAREA * random() - SAREA / 2.0]
+
+
+def animation():
+    link_lengths = [1] * N_LINKS
+    joint_angles = np.array([0] * N_LINKS)
+    goal_pos = get_random_goal()
+    arm = NLinkArm(link_lengths, joint_angles, goal_pos, show_animation)
+    state = WAIT_FOR_NEW_GOAL
+    solution_found = False
+
+    i_goal = 0
+    while True:
+        old_goal = goal_pos
+        goal_pos = arm.goal
+        end_effector = arm.end_effector
+        errors, distance = distance_to_goal(end_effector, goal_pos)
+
+        # State machine to allow changing of goal before current goal has been reached
+        if state is WAIT_FOR_NEW_GOAL:
+
+            if distance > 0.1 and not solution_found:
+                joint_goal_angles, solution_found = inverse_kinematics(
+                    link_lengths, joint_angles, goal_pos)
+                if not solution_found:
+                    print("Solution could not be found.")
+                    state = WAIT_FOR_NEW_GOAL
+                    arm.goal = get_random_goal()
+                elif solution_found:
+                    state = MOVING_TO_GOAL
+        elif state is MOVING_TO_GOAL:
+            if distance > 0.1 and (old_goal is goal_pos):
+                joint_angles = joint_angles + Kp * \
+                    ang_diff(joint_goal_angles, joint_angles) * dt
+            else:
+                state = WAIT_FOR_NEW_GOAL
+                solution_found = False
+                arm.goal = get_random_goal()
+                i_goal += 1
+
+        if i_goal >= 5:
+            break
+
+        arm.update_joints(joint_angles)
+
+
 def forward_kinematics(link_lengths, joint_angles):
     x = y = 0
-    for i in range(1, N_LINKS+1):
-        x += link_lengths[i-1]*np.cos(np.sum(joint_angles[:i]))
-        y += link_lengths[i-1]*np.sin(np.sum(joint_angles[:i]))
+    for i in range(1, N_LINKS + 1):
+        x += link_lengths[i - 1] * np.cos(np.sum(joint_angles[:i]))
+        y += link_lengths[i - 1] * np.sin(np.sum(joint_angles[:i]))
     return np.array([x, y]).T
+
 
 def jacobian_inverse(link_lengths, joint_angles):
     J = np.zeros((2, N_LINKS))
@@ -82,21 +140,25 @@ def jacobian_inverse(link_lengths, joint_angles):
         J[0, i] = 0
         J[1, i] = 0
         for j in range(i, N_LINKS):
-            J[0, i] -= link_lengths[j]*np.sin(np.sum(joint_angles[:j]))
-            J[1, i] += link_lengths[j]*np.cos(np.sum(joint_angles[:j]))
+            J[0, i] -= link_lengths[j] * np.sin(np.sum(joint_angles[:j]))
+            J[1, i] += link_lengths[j] * np.cos(np.sum(joint_angles[:j]))
 
     return np.linalg.pinv(J)
+
 
 def distance_to_goal(current_pos, goal_pos):
     x_diff = goal_pos[0] - current_pos[0]
     y_diff = goal_pos[1] - current_pos[1]
     return np.array([x_diff, y_diff]).T, np.math.sqrt(x_diff**2 + y_diff**2)
 
+
 def ang_diff(theta1, theta2):
     """
     Returns the difference between two angles in the range -pi to +pi
     """
-    return (theta1 - theta2 + np.pi)%(2*np.pi) - np.pi
+    return (theta1 - theta2 + np.pi) % (2 * np.pi) - np.pi
+
 
 if __name__ == '__main__':
-    n_link_arm_ik()
+    main()
+    # animation()
