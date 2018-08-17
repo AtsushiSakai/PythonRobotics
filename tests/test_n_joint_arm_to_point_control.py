e1f97afd301e2166afcaed16d35b53b15eba9594
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-08-17 21:57:56 +0900

    update test code

--- tests/test_n_joint_arm_to_point_control.py
+++ tests/test_n_joint_arm_to_point_control.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from ArmNavigation.n_joint_arm_to_point_control import n_joint_arm_to_point_control as m
 import sys
 sys.path.append("./ArmNavigation/n_joint_arm_to_point_control/")
 
+from ArmNavigation.n_joint_arm_to_point_control import n_joint_arm_to_point_control as m
 print(__file__)
 
 
