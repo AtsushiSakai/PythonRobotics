907c46467db0e6f9b6759a99671be7aa5234b659
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-08-27 20:55:38 +0900

    add Quadrotor animation

--- AerialNavigation/Quadrotor.py
+++ AerialNavigation/Quadrotor.py
@@ -9,12 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 class Quadrotor():
     def __init__(self, x=0, y=0, z=0, roll=0, pitch=0, yaw=0, size=0.25):
-        self.p1 = np.array([size/2, 0, 0, 1]).T
-        self.p2 = np.array([-size/2, 0, 0, 1]).T
-        self.p3 = np.array([0, size/2, 0, 1]).T
-        self.p4 = np.array([0, -size/2, 0, 1]).T
+        self.p1 = np.array([size / 2, 0, 0, 1]).T
+        self.p2 = np.array([-size / 2, 0, 0, 1]).T
+        self.p3 = np.array([0, size / 2, 0, 1]).T
+        self.p4 = np.array([0, -size / 2, 0, 1]).T
 
         self.x_data = []
         self.y_data = []
@@ -47,10 +48,11 @@ class Quadrotor():
         pitch = self.pitch
         yaw = self.yaw
         return np.array(
-            [[cos(yaw)*cos(pitch), -sin(yaw)*cos(roll)+cos(yaw)*sin(pitch)*sin(roll), sin(yaw)*sin(roll)+cos(yaw)*sin(pitch)*cos(roll), x],
-             [sin(yaw)*cos(pitch), cos(yaw)*cos(roll)+sin(yaw)*sin(pitch)*sin(roll), -cos(yaw)*sin(roll)+sin(yaw)*sin(pitch)*cos(roll), y],
-             [-sin(pitch), cos(pitch)*sin(roll), cos(pitch)*cos(yaw), z]
-            ])
+            [[cos(yaw) * cos(pitch), -sin(yaw) * cos(roll) + cos(yaw) * sin(pitch) * sin(roll), sin(yaw) * sin(roll) + cos(yaw) * sin(pitch) * cos(roll), x],
+             [sin(yaw) * cos(pitch), cos(yaw) * cos(roll) + sin(yaw) * sin(pitch) *
+              sin(roll), -cos(yaw) * sin(roll) + sin(yaw) * sin(pitch) * cos(roll), y],
+             [-sin(pitch), cos(pitch) * sin(roll), cos(pitch) * cos(yaw), z]
+             ])
 
     def plot(self):
         T = self.transformation_matrix()
@@ -66,8 +68,10 @@ class Quadrotor():
                      [p1_t[1], p2_t[1], p3_t[1], p4_t[1]],
                      [p1_t[2], p2_t[2], p3_t[2], p4_t[2]], 'k.')
 
-        self.ax.plot([p1_t[0], p2_t[0]], [p1_t[1], p2_t[1]], [p1_t[2], p2_t[2]], 'r-')
-        self.ax.plot([p3_t[0], p4_t[0]], [p3_t[1], p4_t[1]], [p3_t[2], p4_t[2]], 'r-')
+        self.ax.plot([p1_t[0], p2_t[0]], [p1_t[1], p2_t[1]],
+                     [p1_t[2], p2_t[2]], 'r-')
+        self.ax.plot([p3_t[0], p4_t[0]], [p3_t[1], p4_t[1]],
+                     [p3_t[2], p4_t[2]], 'r-')
 
         self.ax.plot(self.x_data, self.y_data, self.z_data, 'b:')
 
@@ -75,5 +79,4 @@ class Quadrotor():
         plt.ylim(-5, 5)
         self.ax.set_zlim(0, 10)
 
-        plt.show()
         plt.pause(0.001)
