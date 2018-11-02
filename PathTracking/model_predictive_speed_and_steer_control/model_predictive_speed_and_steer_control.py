414f459b7d384667d31bc90f31464051fe333539
pengchen <penchen@ucdavis.edu>
2018-11-02 16:28:24 -0700

    small bugs on linear discrete model of MPC

--- PathTracking/model_predictive_speed_and_steer_control/model_predictive_speed_and_steer_control.py
+++ PathTracking/model_predictive_speed_and_steer_control/model_predictive_speed_and_steer_control.py
@@ -97,7 +97,7 @@ def get_linear_model_matrix(v, phi, delta):
     C = np.zeros(NX)
     C[0] = DT * v * math.sin(phi) * phi
     C[1] = - DT * v * math.cos(phi) * phi
-    C[3] = v * delta / (WB * math.cos(delta) ** 2)
+    C[3] = - v * delta / (WB * math.cos(delta) ** 2)
 
     return A, B, C
 
