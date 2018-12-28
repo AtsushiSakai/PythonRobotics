b25f28f318b02dc20c9785b5265fba2f175b8575
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-12-28 23:06:20 +0900

    add rocket powered landing

---
+++ AerialNavigation/rocket_powered_landing/rocket_powered_landing.py
@@ -0,0 +1,17 @@
+""" 
+
+A rocket powered landing with successive convexification
+
+author: Atsushi Sakai
+
+"""
+
+
+def main():
+    print("start!!")
+
+    print("done!!")
+
+
+if __name__ == '__main__':
+    main()
