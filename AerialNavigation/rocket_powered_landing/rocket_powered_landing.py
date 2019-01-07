ff68f2780e46c29589164c558e6ec09e68c265e6
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-01-07 11:58:16 +0900

    code cleanup

--- AerialNavigation/rocket_powered_landing/rocket_powered_landing.py
+++ AerialNavigation/rocket_powered_landing/rocket_powered_landing.py
@@ -38,107 +38,7 @@ verbose_solver = False
 show_animation = True
 
 
-class Integrator:
-    def __init__(self, m, K):
-        self.K = K
-        self.m = m
-        self.n_x = m.n_x
-        self.n_u = m.n_u
-
-        self.A_bar = np.zeros([m.n_x * m.n_x, K - 1])
-        self.B_bar = np.zeros([m.n_x * m.n_u, K - 1])
-        self.C_bar = np.zeros([m.n_x * m.n_u, K - 1])
-        self.S_bar = np.zeros([m.n_x, K - 1])
-        self.z_bar = np.zeros([m.n_x, K - 1])
-
-        # vector indices for flat matrices
-        x_end = m.n_x
-        A_bar_end = m.n_x * (1 + m.n_x)
-        B_bar_end = m.n_x * (1 + m.n_x + m.n_u)
-        C_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u)
-        S_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u + 1)
-        z_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u + 2)
-        self.x_ind = slice(0, x_end)
-        self.A_bar_ind = slice(x_end, A_bar_end)
-        self.B_bar_ind = slice(A_bar_end, B_bar_end)
-        self.C_bar_ind = slice(B_bar_end, C_bar_end)
-        self.S_bar_ind = slice(C_bar_end, S_bar_end)
-        self.z_bar_ind = slice(S_bar_end, z_bar_end)
-
-        self.f, self.A, self.B = m.f_func, m.A_func, m.B_func
-
-        # integration initial condition
-        self.V0 = np.zeros((m.n_x * (1 + m.n_x + m.n_u + m.n_u + 2),))
-        self.V0[self.A_bar_ind] = np.eye(m.n_x).reshape(-1)
-
-        self.dt = 1. / (K - 1)
-
-    def calculate_discretization(self, X, U, sigma):
-        """
-        Calculate discretization for given states, inputs and total time.
-
-        :param X: Matrix of states for all time points
-        :param U: Matrix of inputs for all time points
-        :param sigma: Total time
-        :return: The discretization matrices
-        """
-        for k in range(self.K - 1):
-            self.V0[self.x_ind] = X[:, k]
-            V = np.array(odeint(self._ode_dVdt, self.V0, (0, self.dt),
-                                args=(U[:, k], U[:, k + 1], sigma))[1, :])
-
-            # using \Phi_A(\tau_{k+1},\xi) = \Phi_A(\tau_{k+1},\tau_k)\Phi_A(\xi,\tau_k)^{-1}
-            # flatten matrices in column-major (Fortran) order for CVXPY
-            Phi = V[self.A_bar_ind].reshape((self.n_x, self.n_x))
-            self.A_bar[:, k] = Phi.flatten(order='F')
-            self.B_bar[:, k] = np.matmul(Phi, V[self.B_bar_ind].reshape(
-                (self.n_x, self.n_u))).flatten(order='F')
-            self.C_bar[:, k] = np.matmul(Phi, V[self.C_bar_ind].reshape(
-                (self.n_x, self.n_u))).flatten(order='F')
-            self.S_bar[:, k] = np.matmul(Phi, V[self.S_bar_ind])
-            self.z_bar[:, k] = np.matmul(Phi, V[self.z_bar_ind])
-
-        return self.A_bar, self.B_bar, self.C_bar, self.S_bar, self.z_bar
-
-    def _ode_dVdt(self, V, t, u_t0, u_t1, sigma):
-        """
-        ODE function to compute dVdt.
-
-        :param V: Evaluation state V = [x, Phi_A, B_bar, C_bar, S_bar, z_bar]
-        :param t: Evaluation time
-        :param u_t0: Input at start of interval
-        :param u_t1: Input at end of interval
-        :param sigma: Total time
-        :return: Derivative at current time and state dVdt
-        """
-        alpha = (self.dt - t) / self.dt
-        beta = t / self.dt
-        x = V[self.x_ind]
-        u = u_t0 + beta * (u_t1 - u_t0)
-
-        # using \Phi_A(\tau_{k+1},\xi) = \Phi_A(\tau_{k+1},\tau_k)\Phi_A(\xi,\tau_k)^{-1}
-        # and pre-multiplying with \Phi_A(\tau_{k+1},\tau_k) after integration
-        Phi_A_xi = np.linalg.inv(
-            V[self.A_bar_ind].reshape((self.n_x, self.n_x)))
-
-        A_subs = sigma * self.A(x, u)
-        B_subs = sigma * self.B(x, u)
-        f_subs = self.f(x, u)
-
-        dVdt = np.zeros_like(V)
-        dVdt[self.x_ind] = sigma * f_subs.transpose()
-        dVdt[self.A_bar_ind] = np.matmul(
-            A_subs, V[self.A_bar_ind].reshape((self.n_x, self.n_x))).reshape(-1)
-        dVdt[self.B_bar_ind] = np.matmul(Phi_A_xi, B_subs).reshape(-1) * alpha
-        dVdt[self.C_bar_ind] = np.matmul(Phi_A_xi, B_subs).reshape(-1) * beta
-        dVdt[self.S_bar_ind] = np.matmul(Phi_A_xi, f_subs).transpose()
-        z_t = -np.matmul(A_subs, x) - np.matmul(B_subs, u)
-        dVdt[self.z_bar_ind] = np.dot(Phi_A_xi, z_t.T).flatten()
-
-        return dVdt
-
-
-class Model_6DoF:
+class Rocket_Model_6DoF:
     """
     A 6 degree of freedom rocket landing problem.
     """
@@ -413,6 +313,106 @@ class Model_6DoF:
         return constraints
 
 
+class Integrator:
+    def __init__(self, m, K):
+        self.K = K
+        self.m = m
+        self.n_x = m.n_x
+        self.n_u = m.n_u
+
+        self.A_bar = np.zeros([m.n_x * m.n_x, K - 1])
+        self.B_bar = np.zeros([m.n_x * m.n_u, K - 1])
+        self.C_bar = np.zeros([m.n_x * m.n_u, K - 1])
+        self.S_bar = np.zeros([m.n_x, K - 1])
+        self.z_bar = np.zeros([m.n_x, K - 1])
+
+        # vector indices for flat matrices
+        x_end = m.n_x
+        A_bar_end = m.n_x * (1 + m.n_x)
+        B_bar_end = m.n_x * (1 + m.n_x + m.n_u)
+        C_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u)
+        S_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u + 1)
+        z_bar_end = m.n_x * (1 + m.n_x + m.n_u + m.n_u + 2)
+        self.x_ind = slice(0, x_end)
+        self.A_bar_ind = slice(x_end, A_bar_end)
+        self.B_bar_ind = slice(A_bar_end, B_bar_end)
+        self.C_bar_ind = slice(B_bar_end, C_bar_end)
+        self.S_bar_ind = slice(C_bar_end, S_bar_end)
+        self.z_bar_ind = slice(S_bar_end, z_bar_end)
+
+        self.f, self.A, self.B = m.f_func, m.A_func, m.B_func
+
+        # integration initial condition
+        self.V0 = np.zeros((m.n_x * (1 + m.n_x + m.n_u + m.n_u + 2),))
+        self.V0[self.A_bar_ind] = np.eye(m.n_x).reshape(-1)
+
+        self.dt = 1. / (K - 1)
+
+    def calculate_discretization(self, X, U, sigma):
+        """
+        Calculate discretization for given states, inputs and total time.
+
+        :param X: Matrix of states for all time points
+        :param U: Matrix of inputs for all time points
+        :param sigma: Total time
+        :return: The discretization matrices
+        """
+        for k in range(self.K - 1):
+            self.V0[self.x_ind] = X[:, k]
+            V = np.array(odeint(self._ode_dVdt, self.V0, (0, self.dt),
+                                args=(U[:, k], U[:, k + 1], sigma))[1, :])
+
+            # using \Phi_A(\tau_{k+1},\xi) = \Phi_A(\tau_{k+1},\tau_k)\Phi_A(\xi,\tau_k)^{-1}
+            # flatten matrices in column-major (Fortran) order for CVXPY
+            Phi = V[self.A_bar_ind].reshape((self.n_x, self.n_x))
+            self.A_bar[:, k] = Phi.flatten(order='F')
+            self.B_bar[:, k] = np.matmul(Phi, V[self.B_bar_ind].reshape(
+                (self.n_x, self.n_u))).flatten(order='F')
+            self.C_bar[:, k] = np.matmul(Phi, V[self.C_bar_ind].reshape(
+                (self.n_x, self.n_u))).flatten(order='F')
+            self.S_bar[:, k] = np.matmul(Phi, V[self.S_bar_ind])
+            self.z_bar[:, k] = np.matmul(Phi, V[self.z_bar_ind])
+
+        return self.A_bar, self.B_bar, self.C_bar, self.S_bar, self.z_bar
+
+    def _ode_dVdt(self, V, t, u_t0, u_t1, sigma):
+        """
+        ODE function to compute dVdt.
+
+        :param V: Evaluation state V = [x, Phi_A, B_bar, C_bar, S_bar, z_bar]
+        :param t: Evaluation time
+        :param u_t0: Input at start of interval
+        :param u_t1: Input at end of interval
+        :param sigma: Total time
+        :return: Derivative at current time and state dVdt
+        """
+        alpha = (self.dt - t) / self.dt
+        beta = t / self.dt
+        x = V[self.x_ind]
+        u = u_t0 + beta * (u_t1 - u_t0)
+
+        # using \Phi_A(\tau_{k+1},\xi) = \Phi_A(\tau_{k+1},\tau_k)\Phi_A(\xi,\tau_k)^{-1}
+        # and pre-multiplying with \Phi_A(\tau_{k+1},\tau_k) after integration
+        Phi_A_xi = np.linalg.inv(
+            V[self.A_bar_ind].reshape((self.n_x, self.n_x)))
+
+        A_subs = sigma * self.A(x, u)
+        B_subs = sigma * self.B(x, u)
+        f_subs = self.f(x, u)
+
+        dVdt = np.zeros_like(V)
+        dVdt[self.x_ind] = sigma * f_subs.transpose()
+        dVdt[self.A_bar_ind] = np.matmul(
+            A_subs, V[self.A_bar_ind].reshape((self.n_x, self.n_x))).reshape(-1)
+        dVdt[self.B_bar_ind] = np.matmul(Phi_A_xi, B_subs).reshape(-1) * alpha
+        dVdt[self.C_bar_ind] = np.matmul(Phi_A_xi, B_subs).reshape(-1) * beta
+        dVdt[self.S_bar_ind] = np.matmul(Phi_A_xi, f_subs).transpose()
+        z_t = -np.matmul(A_subs, x) - np.matmul(B_subs, u)
+        dVdt[self.z_bar_ind] = np.dot(Phi_A_xi, z_t.T).flatten()
+
+        return dVdt
+
+
 class SCProblem:
     """
     Defines a standard Successive Convexification problem and
@@ -605,7 +605,7 @@ def plot_animation(X, U):
 
 def main():
     print("start!!")
-    m = Model_6DoF()
+    m = Rocket_Model_6DoF()
 
     # state and input list
     X = np.empty(shape=[m.n_x, K])
