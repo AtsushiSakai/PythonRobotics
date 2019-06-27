02c197d6991254c8c48b4f9817b86bc7c2e7c00c
Zhao Zihe <2809075376@qq.com>
2019-06-27 17:36:37 +0800

    patch the yaw=0 direction while expanding

--- PathPlanning/HybridAStar/hybrid_a_star.py
+++ PathPlanning/HybridAStar/hybrid_a_star.py
@@ -131,7 +131,7 @@ class Config:
 
 def calc_motion_inputs():
 
-    for steer in np.linspace(-MAX_STEER, MAX_STEER, N_STEER):
+    for steer in np.concatenate((np.linspace(-MAX_STEER, MAX_STEER, N_STEER),[0.0])):
         for d in [1, -1]:
             yield [steer, d]
 
