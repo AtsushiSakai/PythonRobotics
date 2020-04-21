72ed1d9a2bd1ca637d2dec666df4316af5820a17
Ohiogeek8215 <63999011+Ohiogeek8215@users.noreply.github.com>
2020-04-21 04:06:48 -0400

    Delete runtests.sh

--- runtests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Run test suites! "
-#python -m unittest discover tests 
-#python -Wignore -m unittest discover tests #ignore warning
-coverage run -m unittest discover tests # generate coverage file
