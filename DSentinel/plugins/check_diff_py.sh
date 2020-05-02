68472377226b201cda958f10c515818e1c158ce8
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-02 20:51:48 +0900

    implement git diff

--- DSentinel/plugins/check_diff_py.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function check_diff_py(){
-    echo "check_diff_py"
-    git diff --unified=0 $1 -- ${GIT_ROOT_DIR}'/*'.py | pycodestyle --diff | cat
-    git diff --unified=0 084c86244798f67fd1fbd29212a9ea36e67e3a97 -- *py | pycodestyle --diff
-    echo ${PIPESTATUS[@]}
-#    git diff --unified=0 $1 -- ${GIT_ROOT_DIR}'/*'.py | pycodestyle --diff | echo $(cat)
-}
