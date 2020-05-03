7d43acd7acc0d6a2ceba83223d081a12cf02dea8
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-03 22:17:57 +0900

    implement git diff

--- rundiffstylecheck.sh
+++ rundiffstylecheck.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "$(basename $0) start!"
-VERSION=v0.1.1
+VERSION=v0.1.3
 wget https://github.com/AtsushiSakai/DiffSentinel/archive/${VERSION}.zip
 unzip ${VERSION}.zip
-./DiffSentinel*/starter.sh
+./DiffSentinel*/starter.sh HEAD remotes/origin/master
 rm -rf ${VERSION}.zip DiffSentinel*
 echo "$(basename $0) done!"
 exit 0
