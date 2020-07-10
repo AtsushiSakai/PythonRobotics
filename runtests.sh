5134e81679d8f32ab6322d0f4ee0a4a1a8a93843
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-07-10 22:22:19 +0900

    add python warning setting

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Run test suites! "
+export PYTHONWARNINGS=default  # show warning
 #python -m unittest discover tests 
 #python -Wignore -m unittest discover tests #ignore warning
 coverage run -m unittest discover tests # generate coverage file
