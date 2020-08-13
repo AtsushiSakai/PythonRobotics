c2f1d293066bb6a23f2d8647b34154228947a357
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2020-08-13 11:34:48 +0530

    Try2

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -3,26 +3,15 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
                 "/../PathPlanning/DepthFirstSearch/")
-
-
 try:
     import depth_first_search as m
 except ImportError:
-	raise	
-
-
-print(__file__)
-
-
+	raise
+print(__file__) 
 class Test(TestCase):
-
-    def test1(self):
+	def test1(self):
         m.show_animation = False
         m.main()
-
-
 if __name__ == '__main__':  # pragma: no cover
     test = Test()
-    test.test1()
-
-    
+    test.test1()     
--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,26 +1,17 @@
 from unittest import TestCase
-
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
                 "/../PathPlanning/Dijkstra/")
-
 try:
     import dijkstra as m
 except ImportError:
 	raise
-
-print(__file__)
-
-
+print(__file__) 
 class Test(TestCase):
-
-    def test1(self):
+	def test1(self):
         m.show_animation = False
         m.main()
-
 if __name__ == '__main__':  # pragma: no cover
     test = Test()
-    test.test1()
-
-    
+    test.test1()     
