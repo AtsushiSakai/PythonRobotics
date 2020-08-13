1e70300b28ed4edea36486d920230e73379ff147
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2020-08-13 11:21:55 +0530

    Try to remove CI error

--- tests/test_breadth_first_search.py
+++ tests/test_breadth_first_search.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
 
 try:
     import breadth_first_search as m
-except ImportError :
+except ImportError:
 	raise
     
 
--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
 
 try:
     import dijkstra as m
-except ImportError :
+except ImportError:
 	raise
 
 print(__file__)
