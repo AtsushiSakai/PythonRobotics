6de00e18afe55ca4a1ce105490504e224331a625
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2020-08-14 15:02:06 +0530

    Check1

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
 try:
     import depth_first_search as m
 except ImportError:
-	 raise
+	raise
 
 
 print(__file__)
@@ -16,11 +16,11 @@ print(__file__)
 
 class Test(TestCase):
 
-    def test1(self):
+	def test1(self):
         m.show_animation = False
         m.main()
 
 
 if __name__ == '__main__':  # pragma: no cover
     test = Test()
-    test.test1() 
+    test.test1()
