6249f63fb2df3112b1bc48d55ccdbd8059ea1d6c
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2020-08-14 14:10:07 +0530

    Another Day

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -3,20 +3,24 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
                 "/../PathPlanning/DepthFirstSearch/")
+
+
 try:
     import depth_first_search as m
 except ImportError:
-	raise
+	 raise
+
+
 print(__file__)
 
 
 class Test(TestCase):
 
+    def test1(self):
+        m.show_animation = False
+        m.main()
 
- def test1(self):
-  m.show_animation = False
-  m.main()
+
 if __name__ == '__main__':  # pragma: no cover
- test = Test()
-
- test.test1() 
+    test = Test()
+    test.test1() 
