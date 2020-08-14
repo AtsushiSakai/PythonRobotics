318779a5576b6f3275ba365a0a8749de5ecb9c71
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2020-08-14 15:07:20 +0530

    check3

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
 try:
     import depth_first_search as m
 except ImportError:
-	raise
+    raise
 
 
 print(__file__)
@@ -16,7 +16,7 @@ print(__file__)
 
 class Test(TestCase):
 
-	def test1(self):
+    def test1(self):
         m.show_animation = False
         m.main()
 
