766525879e4bea179c03b4569dc792befd4880d9
Sarim Mehdi <sarim.mehdi.550@gmail.com>
2020-08-29 07:39:02 +0200

    Add files via upload

---
+++ tests/test_bug.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+import sys
+import os
+sys.path.append(os.path.dirname(__file__) + "/../")
+try:
+    import bug as b
+except:
+    raise
+
+
+class Test(TestCase):
+
+    def test(self):
+        b.show_animation = False
+        b.main(bug0=True, bug1=True, bug2=True)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    test = Test()
+    test.test()
