21b8b414ae37674bb4ae368e5c46d19cfde3468e
weicent <weicent_zhang@outlook.com>
2020-09-04 16:05:15 +0800

    add test file

--- tests/test_a_star_searching_two_side.py
+++ tests/test_a_star_searching_two_side.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
-import os, sys
+import os
+import sys
 
-sys.path.append(os.path.dirname(__file__)+'/../')
+sys.path.append(os.path.dirname(__file__) + '/../')
 
 try:
     from PathPlanning.AStar import A_Star_searching_from_two_side as m
-except:
+except ImportError:
     raise
 
 
@@ -15,6 +16,7 @@ class Test(TestCase):
         m.show_animation = False
         m.main([60, 60], [0, 0], 1500)
 
-if __name__=='__main__':
-    test=Test()
+
+if __name__ == '__main__':
+    test = Test()
     test.test1()
