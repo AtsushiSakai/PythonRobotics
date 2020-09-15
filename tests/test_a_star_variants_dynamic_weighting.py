a2fc5241e0279216331a28665003d9f7510c58b5
Sarim Mehdi <sarim.mehdi.550@gmail.com>
2020-09-15 06:43:43 +0200

    Add files via upload

---
+++ tests/test_a_star_variants_dynamic_weighting.py
@@ -0,0 +1,18 @@
+import a_star_variants as astar
+from unittest import TestCase
+import sys
+import os
+sys.path.append(os.path.dirname(__file__) + "/../")
+
+
+class Test(TestCase):
+
+    def test(self):
+        # A* with dynamic weighting
+        astar.use_dynamic_weighting = True
+        astar.main()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    test = Test()
+    test.test()
---
+++ tests/test_a_star_variants_jump_point.py
@@ -0,0 +1,18 @@
+import a_star_variants as astar
+from unittest import TestCase
+import sys
+import os
+sys.path.append(os.path.dirname(__file__) + "/../")
+
+
+class Test(TestCase):
+
+    def test(self):
+        # A* with jump point
+        astar.use_jump_point = True
+        astar.main()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    test = Test()
+    test.test()
