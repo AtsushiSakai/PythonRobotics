aa3b2f1685d90ea91a08097a76db6b3380abe7d2
Pandacoolcool <1308455330@qq.com>
2020-10-13 10:12:52 +0800

    Update extended_kalman_filter.py
    Synchronization: Jacobian of Motion Model

--- Localization/extended_kalman_filter/extended_kalman_filter.py
+++ Localization/extended_kalman_filter/extended_kalman_filter.py
@@ -56,7 +56,7 @@ def motion_model(x, u):
     F = np.array([[1.0, 0, 0, 0],
                   [0, 1.0, 0, 0],
                   [0, 0, 1.0, 0],
-                  [0, 0, 0, 0]])
+                  [0, 0, 0, 1.0]])
 
     B = np.array([[DT * math.cos(x[2, 0]), 0],
                   [DT * math.sin(x[2, 0]), 0],
