c6fa8b2973cc3fad07a1a4687a7ba92e51012503
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-10 22:43:01 +0900

    Test code clean up

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,7 @@
 import sys
 import os
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(TEST_DIR)
+ROOT_DIR = os.path.dirname(TEST_DIR)
 sys.path.append(ROOT_DIR)
