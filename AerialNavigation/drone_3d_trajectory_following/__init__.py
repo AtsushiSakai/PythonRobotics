f19548acfd0ba93a2f83f285e5bb260784892d9d
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-11 13:48:42 +0900

    Test code clean up

---
+++ AerialNavigation/drone_3d_trajectory_following/__init__.py
@@ -0,0 +1,3 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
--- tests/test_dubins_path_planning.py
+++ tests/test_dubins_path_planning.py
@@ -1,60 +1,61 @@
+import conftest  # Add root path to sys.path
 from unittest import TestCase
-
+from PathPlanning.DubinsPath import dubins_path_planning
 import numpy as np
 np.random.seed(12345)
 
-from PathPlanning.DubinsPath import dubins_path_planning
+
+def test_check_edge_condition(px, py, pyaw, start_x, start_y, start_yaw,
+                              end_x, end_y, end_yaw):
+    assert (abs(px[0] - start_x) <= 0.01)
+    assert (abs(py[0] - start_y) <= 0.01)
+    assert (abs(pyaw[0] - start_yaw) <= 0.01)
+    assert (abs(px[-1] - end_x) <= 0.01)
+    assert (abs(py[-1] - end_y) <= 0.01)
+    assert (abs(pyaw[-1] - end_yaw) <= 0.01)
 
 
-class Test(TestCase):
+def test_1(self):
+    start_x = 1.0  # [m]
+    start_y = 1.0  # [m]
+    start_yaw = np.deg2rad(45.0)  # [rad]
 
-    @staticmethod
-    def check_edge_condition(px, py, pyaw, start_x, start_y, start_yaw, end_x, end_y, end_yaw):
-        assert (abs(px[0] - start_x) <= 0.01)
-        assert (abs(py[0] - start_y) <= 0.01)
-        assert (abs(pyaw[0] - start_yaw) <= 0.01)
-        print("x", px[-1], end_x)
-        assert (abs(px[-1] - end_x) <= 0.01)
-        print("y", py[-1], end_y)
-        assert (abs(py[-1] - end_y) <= 0.01)
-        print("yaw", pyaw[-1], end_yaw)
-        assert (abs(pyaw[-1] - end_yaw) <= 0.01)
+    end_x = -3.0  # [m]
+    end_y = -3.0  # [m]
+    end_yaw = np.deg2rad(-45.0)  # [rad]
 
-    def test1(self):
-        start_x = 1.0  # [m]
-        start_y = 1.0  # [m]
-        start_yaw = np.deg2rad(45.0)  # [rad]
+    curvature = 1.0
 
-        end_x = -3.0  # [m]
-        end_y = -3.0  # [m]
-        end_yaw = np.deg2rad(-45.0)  # [rad]
+    px, py, pyaw, mode, clen = dubins_path_planning.dubins_path_planning(
+        start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature)
 
-        curvature = 1.0
+    self.check_edge_condition(px, py, pyaw,
+                              start_x, start_y, start_yaw,
+                              end_x, end_y, end_yaw)
+
+
+def test_2():
+    dubins_path_planning.show_animation = False
+    dubins_path_planning.main()
+
+
+def test_3():
+    N_TEST = 10
+
+    for i in range(N_TEST):
+        start_x = (np.random.rand() - 0.5) * 10.0  # [m]
+        start_y = (np.random.rand() - 0.5) * 10.0  # [m]
+        start_yaw = np.deg2rad((np.random.rand() - 0.5) * 180.0)  # [rad]
+
+        end_x = (np.random.rand() - 0.5) * 10.0  # [m]
+        end_y = (np.random.rand() - 0.5) * 10.0  # [m]
+        end_yaw = np.deg2rad((np.random.rand() - 0.5) * 180.0)  # [rad]
+
+        curvature = 1.0 / (np.random.rand() * 5.0)
 
         px, py, pyaw, mode, clen = dubins_path_planning.dubins_path_planning(
             start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature)
 
-        self.check_edge_condition(px, py, pyaw, start_x, start_y, start_yaw, end_x, end_y, end_yaw)
-
-    def test2(self):
-        dubins_path_planning.show_animation = False
-        dubins_path_planning.main()
-
-    def test3(self):
-        N_TEST = 10
-
-        for i in range(N_TEST):
-            start_x = (np.random.rand() - 0.5) * 10.0  # [m]
-            start_y = (np.random.rand() - 0.5) * 10.0  # [m]
-            start_yaw = np.deg2rad((np.random.rand() - 0.5) * 180.0)  # [rad]
-
-            end_x = (np.random.rand() - 0.5) * 10.0  # [m]
-            end_y = (np.random.rand() - 0.5) * 10.0  # [m]
-            end_yaw = np.deg2rad((np.random.rand() - 0.5) * 180.0)  # [rad]
-
-            curvature = 1.0 / (np.random.rand() * 5.0)
-
-            px, py, pyaw, mode, clen = dubins_path_planning.dubins_path_planning(
-                start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature)
-
-            self.check_edge_condition(px, py, pyaw, start_x, start_y, start_yaw, end_x, end_y, end_yaw)
+        self.check_edge_condition(px, py, pyaw,
+                                  start_x, start_y, start_yaw,
+                                  end_x, end_y, end_yaw)
