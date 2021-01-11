3fc7efce067146ffd608d07190ded4aca62c5e4e
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-11 14:10:35 +0900

    Test code clean up

--- tests/test_closed_loop_rrt_star_car.py
+++ tests/test_closed_loop_rrt_star_car.py
@@ -1,11 +1,9 @@
-import conftest  # Add root path to sys.path
-from PathPlanning.ClosedLoopRRTStar import closed_loop_rrt_star_car as m
 import random
 
+from PathPlanning.ClosedLoopRRTStar import closed_loop_rrt_star_car as m
 
-class Test(TestCase):
 
-    def test1(self):
-        random.seed(12345)
-        m.show_animation = False
-        m.main(gx=1.0, gy=0.0, gyaw=0.0, max_iter=5)
+def test_1():
+    random.seed(12345)
+    m.show_animation = False
+    m.main(gx=1.0, gy=0.0, gyaw=0.0, max_iter=5)
