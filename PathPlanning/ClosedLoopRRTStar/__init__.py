0df6b588fd1cf18db09102f7c85e2b98172f8d1c
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-11 14:24:58 +0900

    Test code clean up

--- PathPlanning/ClosedLoopRRTStar/__init__.py
+++ PathPlanning/ClosedLoopRRTStar/__init__.py
@@ -6,4 +6,3 @@ sys.path.append(os.path.dirname(
     os.path.abspath(__file__)) + "/../ReedsSheppPath/")
 sys.path.append(os.path.dirname(
     os.path.abspath(__file__)) + "/../RRTStarReedsShepp/")
-
