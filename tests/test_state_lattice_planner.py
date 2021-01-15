3dff75aeddd39d0d5ddc33d333ec4809c329c27a
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-15 21:53:35 +0900

    Remove unnecessary plot

--- tests/test_state_lattice_planner.py
+++ tests/test_state_lattice_planner.py
@@ -1,10 +1,7 @@
 import conftest  # Add root path to sys.path
 from PathPlanning.StateLatticePlanner import state_lattice_planner as m
-from PathPlanning.ModelPredictiveTrajectoryGenerator \
-    import model_predictive_trajectory_generator as m2
 
 
 def test1():
     m.show_animation = False
-    m2.show_animation = False
     m.main()
