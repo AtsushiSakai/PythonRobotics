92005599c49abe4a2849d512077abe45467a8fc8
FXCarl <fxcarlcn@gmail.com>
2021-02-06 21:24:49 +0800

    Update greedy_best_first_search.py
    parent_index should be pind. other wise it cann't work

--- PathPlanning/GreedyBestFirstSearch/greedy_best_first_search.py
+++ PathPlanning/GreedyBestFirstSearch/greedy_best_first_search.py
@@ -132,7 +132,7 @@ class BestFirstSearchPlanner:
         # generate final course
         rx, ry = [self.calc_grid_position(ngoal.x, self.minx)], [
             self.calc_grid_position(ngoal.y, self.miny)]
-        n = closedset[ngoal.parent_index]
+        n = closedset[ngoal.pind]
         while n is not None:
             rx.append(self.calc_grid_position(n.x, self.minx))
             ry.append(self.calc_grid_position(n.y, self.miny))
