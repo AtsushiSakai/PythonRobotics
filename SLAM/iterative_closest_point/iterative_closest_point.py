56b319c7144a8ec8b16bd35ba7dc1d6e999ad535
Shamil GEMUEV <https://github.maf-roda.com/>
2021-02-11 17:34:25 +0100

    Style correction
    * remove space

--- SLAM/iterative_closest_point/iterative_closest_point.py
+++ SLAM/iterative_closest_point/iterative_closest_point.py
@@ -44,7 +44,7 @@ def icp_matching(previous_points, current_points):
                 fig = plt.figure()
                 ax = fig.add_subplot(111, projection='3d')
                 ax.scatter(previous_points[0, :], previous_points[1, :],
-                            previous_points[2, :], c="r", marker=".")
+                           previous_points[2, :], c="r", marker=".")
                 ax.scatter(current_points[0, :], current_points[1, :],
                            current_points[2, :], c="b", marker=".")
                 ax.scatter(0.0, 0.0, 0.0, c="r", marker="x")
