3715a07a7c70b70b591d1566e23cf82b4f714d50
Shamil GEMUEV <https://github.maf-roda.com/>
2021-02-11 16:15:39 +0100

    Add test for ICP 3d

--- tests/test_iterative_closest_point.py
+++ tests/test_iterative_closest_point.py
@@ -6,6 +6,9 @@ def test_1():
     m.show_animation = False
     m.main()
 
+def test_2():
+    m.show_animation = False
+    m.main_3d_points()
 
 if __name__ == '__main__':
     conftest.run_this_test(__file__)
