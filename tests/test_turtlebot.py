5345501b69798c1d6ff4787b8bdeb6cf45c76304
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-08 17:24:46 +0200

    removed spaces

--- tests/test_turtlebot.py
+++ tests/test_turtlebot.py
@@ -3,7 +3,7 @@
 Test turtlebot simulator
 
 to run this test first install the PythonRobotics as development version.
-do this by running 
+do this by running
 >pip install -e .
 from the main PythonRobotics directory.
 This removes the need for the `conftest` hack ;-).
@@ -13,8 +13,7 @@ author - Jev Kuznetsov
 
 from PythonRobotics.PythonRobotics import turtlebot as m
 
+
 def test_1():
     m.show_animation = False
     m.main()
-
-
