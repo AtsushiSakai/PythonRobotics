1627064f56b5896eff3bd98b76a66af63ae63c7f
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-08 16:41:19 +0200

    added test

---
+++ tests/test_turtlebot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Test turtlebot simulator
+
+to run this test first install the PythonRobotics as development version.
+do this by running 
+>pip install -e .
+from the main PythonRobotics directory.
+This removes the need for the `conftest` hack ;-).
+
+author - Jev Kuznetsov
+"""
+
+from PythonRobotics.PythonRobotics import turtlebot as m
+
+def test_1():
+    m.show_animation = False
+    m.main()
+
+
