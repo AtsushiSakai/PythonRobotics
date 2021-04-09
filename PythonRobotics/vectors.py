1597a63ee3c768e75a973e264fdca983a0ed2b83
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-09 15:38:00 +0200

    added vectors class

---
+++ PythonRobotics/vectors.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""
+Simple vector classes. (Python3)
+
+Why not use numpy instead? A couple of reasons:
+1. performance, working with small numpy arrays introduces a lot of overhead
+2. .x and .y class properties
+3. clear implementation for readability and porting to other languages.
+
+ Copyright (c) 2021 AIGRO B.V. - Jev Kuznetsov
+ License: BSD
+
+Special thanks for these information sources and references:
+
+* [vector.py](https://gist.github.com/mostley/3819375)
+* [StackOverflow](https://stackoverflow.com/a/43542669)
+* [vector.py](https://github.com/betados/vector_2d/blob/develop/vector_2d/vector.py)
+
+"""
+import numpy as np
+from math import atan2, hypot, pi, acos
+
+
+class Vector2d:
+    """ 2d vector, compatible with numpy """
+
+    def __init__(self, *args):
+        """ create from x,y or xy """
+        try:
+            self.x, self.y = float(args[0]), float(args[1])
+        except:
+            self.x = float(args[0][0])
+            self.y = float(args[0][1])
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(x={self.x}, y={self.y})"
+
+    def __add__(self, other):
+        return Vector2d(self.x+other.x, self.y+other.y)
+
+    def __sub__(self, other):
+        return Vector2d(self.x-other.x, self.y-other.y)
+
+    def __eq__(self, other):
+        return (self.x == other.x) & (self.y == other.y)
+
+    def cross(self, other):
+        return (self.x * other.y) - (self.y * other.x)
+
+    def inner(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def __mul__(self, scalar):
+        """ scalar multiplification """
+        return Vector2d(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar):  # note __div__ was removed in Python3
+        return Vector2d(self.x / scalar, self.y / scalar)
+
+    def __neg__(self):
+        return Vector2d(-self.x, -self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __array__(self, dtype=None) -> np.array:
+        """ make this class compatable with numpy operations """
+        if dtype:
+            return np.array([self.x, self.y], dtype=dtype)
+        else:
+            return np.array([self.x, self.y])
---
+++ tests/test_vectors.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Test vector module
+
+author - Jev Kuznetsov
+"""
+
+
+from PythonRobotics.vectors import Vector2d
+import numpy as np
+
+# test tuples
+ta = (1, 2)
+tb = (3, 4)
+
+a = Vector2d(ta)
+b = Vector2d(tb)
+c = Vector2d(1, 0)
+d = Vector2d(0, 1)
+e = Vector2d(1, 1)
+
+
+def test_creation():
+    an = np.array(ta)
+    v = Vector2d(ta)
+    assert (an == v.__array__()).all()
+
+    v = Vector2d(an)
+    assert (an == v.__array__()).all()
+
+
+def test_add():
+    assert a + b == Vector2d(4, 6)
+
+
+def test_cross():
+    assert np.cross(a, b) == a.cross(b)
+    assert np.cross(c, d) == c.cross(d)
+
+
+def test_sub():
+    assert a - b == Vector2d(-2, -2)
+
+
+def test_inner():
+    assert (a.inner(b) == np.inner(ta, tb)).all()
+
+
+def test_mul():
+    assert a * 3 == Vector2d(3, 6)
+
+def test_div():
+    a = Vector2d(1,2)
+    assert a / 2 == Vector2d(0.5,1.)
+
+def test_neg():
+    assert -a == Vector2d(-1,-2)
+
+def test_length():
+    assert abs(c) == 1
+    assert abs(d) == 1
+    assert abs(e) == np.sqrt(2)
