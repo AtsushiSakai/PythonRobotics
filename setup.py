c5776b0ee12359611f1364c2fb94b915c50052d0
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-10 12:25:03 +0200

    minor tidying up

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-      name='PythonRobotics',
-      version = '1.0.1',
-      description="Python implementation of robotics algoritms",
-      author= "Atsushi Sakai et al.",
-      packages=find_packages(include=['PythonRobotics']),
-      url = "https://github.com/AtsushiSakai/PythonRobotics",
-      install_requires=[],
-      license='MIT'
-      )
+    name='PythonRobotics',
+    version='1.0.1',
+    description="Python implementation of robotics algoritms",
+    author="Atsushi Sakai et al.",
+    packages=find_packages(include=['PythonRobotics']),
+    url="https://github.com/AtsushiSakai/PythonRobotics",
+    install_requires=[],
+    license='MIT'
+)
