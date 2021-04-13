958c18c8feb05a9a84fe1fc6b1d7db5a3180c999
Jev Kuznetsov <jev@aigro.nl>
2021-04-13 12:19:01 +0200

    Squashed commit of the following:
    commit 6a720376fed65aab691284c9e55f4c2f483ef1f4
    Author: Jev Kuznetsov <jev@aigro.nl>
    Date:   Tue Apr 13 12:15:38 2021 +0200
    
        renamed parameter
    
    commit 648249d3df25ddda95fe1b8abdf921c2a43fb091
    Author: Jev Kuznetsov <jev@aigro.nl>
    Date:   Tue Apr 13 12:14:11 2021 +0200
    
        moved animation inside the simulation
    
    commit 89cb4d71c143748a9cfcd3f068d512bdf6c81dcd
    Author: Jev Kuznetsov <jev@aigro.nl>
    Date:   Tue Apr 13 11:45:34 2021 +0200
    
        added turtle graphics
    
    commit ca5ff1d1364065e3aa7e57bf8f35664af80a4f4e
    Author: Jev Kuznetsov <jev.kuznetsov@gmail.com>
    Date:   Mon Apr 12 22:51:16 2021 +0200
    
        style
    
    commit 60b92dc9f722bbbcfdadf23d2bae5222b2b44b00
    Author: Jev Kuznetsov <jev.kuznetsov@gmail.com>
    Date:   Mon Apr 12 22:45:43 2021 +0200
    
        changed states interface
    
    commit 236ac68ea87656bdc8a839f8e6cb50236f564457
    Author: Jev Kuznetsov <jev.kuznetsov@gmail.com>
    Date:   Mon Apr 12 22:22:32 2021 +0200
    
        changed state to function

--- PythonRobotics/turtlebot.py
+++ PythonRobotics/turtlebot.py
@@ -8,12 +8,16 @@ Author  - Jev Kuznetsov
 """
 
 from collections import namedtuple
-from math import sin, cos
+from math import sin, cos, degrees
+import turtle
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 show_animation = True
+plot_result = False
+plot_scale = 1000
 
 # define state object
 state_fields = ['x', 'y', 'phi', 'v', 'omega', 't']
@@ -36,13 +40,18 @@ class Robot:
                  x=0.,  # x-position
                  y=0.,
                  phi=0.,
-                 name='Turtlebot3'):
+                 animate=False):
         """ initiate robot at given location and orientation """
-        self.name = name
         self.states = [State(x, y, phi)]
-
         self._omega_target = 0.
         self._v_target = 0.
+
+        if animate:
+            self.screen = turtle.Screen()
+            self.screen.setup(640, 480)
+            self.turtle = turtle.Turtle()
+        else:
+            self.turtle = None
 
     def set_velocity(self, v, omega):
         """ set target velocities """
@@ -80,7 +89,10 @@ class Robot:
 
         self.states.append(s_new)
 
-
+        # animate if required
+        if self.turtle is not None:
+            self.turtle.setpos(s_new.x*plot_scale, s_new.y * plot_scale)
+            self.turtle.setheading(degrees(s_new.phi))
 
     def states_df(self):
         """ states as DataFrame """
@@ -101,19 +113,19 @@ class Robot:
         return self.states[-1]
 
     def __repr__(self):
-        return f'{self.name} {self.state})'
+        return f'Turtlebot {self.state})'
 
 
 def main():
     """ demonstrate functionality """
-    
-    bot = Robot()
+
+    sim = Robot(animate=show_animation)
 
     # high acceleration ~ instantaneous velocity
-    bot.accel_angular = 1000  # high angular acceleration
-    bot.accel_linear = 1  # constant linear acceleration
-    bot.v_max = 10  # max linear velocity
-    bot.omega_max = 2*np.pi  # max angular velocity
+    sim.accel_angular = 1000  # high angular acceleration
+    sim.accel_linear = 1  # constant linear acceleration
+    sim.v_max = 10  # max linear velocity
+    sim.omega_max = 2*np.pi  # max angular velocity
 
     omega = 2*np.pi  # rad/s
 
@@ -122,16 +134,16 @@ def main():
     dt = 0.01  # timestep
     n_steps = int(t_sim / dt)
 
-    bot.set_velocity(1.0, omega)
+    sim.set_velocity(1.0, omega)
 
     for _ in range(n_steps):
-        bot.step(dt)
+        sim.step(dt)
 
-    states = bot.states_df()
+    states = sim.states_df()
     print('Simulation result:\n', states)
 
     # plot data
-    if show_animation:
+    if plot_result:
         plt.style.use('seaborn-whitegrid')
 
         plt.cla()
@@ -160,6 +172,10 @@ def main():
         plt.subplots_adjust(hspace=0.4)
 
         plt.show()
+
+    if show_animation:
+        print('close plot window to continue....')
+        turtle.done()
 
 
 if __name__ == '__main__':
