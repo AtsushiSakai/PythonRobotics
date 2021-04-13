474638c5bd46c0ad7f688e8035279c7d98f8489e
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-13 20:30:39 +0200

    added target position calculation

--- PathTracking/pure_pursuit/pure_pursuit_2.py
+++ PathTracking/pure_pursuit/pure_pursuit_2.py
@@ -5,6 +5,7 @@
  Copyright (c) 2021 AIGRO B.V. - Jev Kuznetsov
 """
 
+from collections import UserList
 import time
 import math
 import turtle
@@ -16,12 +17,13 @@ from PythonRobotics.graphics import World
 show_animation = True
 
 
-class Waypoints:
+class Waypoints(UserList):
     """ navigation waypoints, collection of xy Vectors """
 
-    def __init__(self, points: 'list[Vector]'):
+    def __init__(self, *args, **kwargs):
         """ create waypoints. """
-        self.points = [Vector(x, y) for x, y in points]
+        super().__init__(*args, **kwargs)
+
         self.next_idx = None
 
     def find_next_idx(self, xy: 'Vector') -> 'int':
@@ -29,14 +31,14 @@ class Waypoints:
 
         if self.next_idx is None:  # first time search
             # distance between points
-            dst = [pt.distance(xy) for pt in self.points]
+            dst = [pt.distance(xy) for pt in self.data]
             idx = np.argmin(dst)
             self.next_idx = idx + 1
         else:
             # calculate distances to n and n+1 waypoints
             idx = self.next_idx
-            dst_a = xy.distance(self.points[idx-1])
-            dst_b = xy.distance(self.points[idx])
+            dst_a = xy.distance(self.data[idx-1])
+            dst_b = xy.distance(self.data[idx])
             if dst_a > dst_b:  # next point
                 self.next_idx += 1
 
@@ -44,7 +46,7 @@ class Waypoints:
 
     @property
     def target_reached(self):
-        return not self.next_idx < len(self.points)
+        return not self.next_idx < len(self.data)
 
 
 def point_on_line(a, b, p):
@@ -55,32 +57,49 @@ def point_on_line(a, b, p):
     return result
 
 
+def target_position(state, wp_a, wp_b,  look_ahead=1) -> 'Vector':
+    """ calculate robot target position """
+
+    # future position
+    d = Vector(state.x, state.y) + Vector.from_polar(look_ahead, state.phi)
+
+    # project to line
+    e = point_on_line(wp_a, wp_b, d)
+
+    return e
+
+
 def main():
 
     world = World((-1, -1, 3, 3))
+    world.add_marker('target', trace=True)
 
     # sim parameters
     t_sim = 10.0  # simulation time [sec]
     dt = 0.1  # timestep
     n_steps = int(t_sim / dt)
-
     # create waypoints
-    coord = [(0, 0), (0, 2), (2, 2), (2, 0), (2, -5)]
+    coord = [Vector(xy) for xy in [(0, 0), (0, 2), (2, 2), (2, 0), (2, -5)]]
     world.plot_path(coord)
-    wp = Waypoints(coord)
-    print('Waypoints: ', wp.points)
+    waypoints = Waypoints(coord)
+    print('Waypoints: ', waypoints)
 
     # create robot
     bot = Robot(phi=math.pi/4)
-    bot.plot_scale = 100
     bot.set_velocity(3., math.pi/2)
 
     # simulate
-    for _ in range(n_steps):
+    for i_step in range(n_steps):
         bot.step(dt)
-        world.robot_step(bot.state)
 
-        time.sleep(0.01)
+        idx = waypoints.find_next_idx(bot.xy)
+        target = target_position(bot.state, waypoints[idx-1], waypoints[idx])
+
+        world.move_robot(bot.state)
+
+        world.move_marker('target', target)
+        print(f"step[{i_step}] next_wp:{waypoints.next_idx}")
+        time.sleep(0.1)
 
     print('Simulation result:\n', bot.states_df())
 
