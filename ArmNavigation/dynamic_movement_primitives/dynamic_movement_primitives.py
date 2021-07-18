4372e74e0b0eede9db74c7981c8b282309931aed
SchwartzCode <jonbs98@gmail.com>
2021-07-18 19:19:20 -0400

    Multi-dimension path learning (e.g. in x and y instead of just x)

--- ArmNavigation/dynamic_movement_primitives/dynamic_movement_primitives.py
+++ ArmNavigation/dynamic_movement_primitives/dynamic_movement_primitives.py
@@ -1,119 +1,157 @@
-'''
+"""
 Author: Jonathan Schwartz (github.com/SchwartzCode)
 More information on Dynamic Movement Primitives available at:
 https://arxiv.org/abs/2102.03861
 https://www.frontiersin.org/articles/10.3389/fncom.2013.00138/full
-'''
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 class DMP(object):
 
-    def __init__(self, K=156.25, B=25, dt=0.001, timesteps=5000,
-                 training_data=None):
+    def __init__(self, training_data, data_period, K=156.25, B=25, dt=0.001,
+                 timesteps=5000):
+        """
+        Arguments:
+            training_data - data in for [(x1,y1), (x2,y2), ...]
+            data_period   - amount of time training data covers
+            K and B       - spring and damper constants to define DMP behavior
+            dt            - timestep between points in generated trajectories
+            timesteps     - number of points in generated trajectories
+        """
         self.K = K  # virtual spring constant
         self.B = B  # virtual damper coefficient
 
         self.dt = dt
         self.timesteps = timesteps
 
-        if training_data is None:
-            self.training_data = []
-            self.train_t_vals = np.linspace(0, np.pi, 150)
+        self.weights = None  # weights used to generate DMP trajectories
 
-            for t in self.train_t_vals[:125]:
-                self.training_data.append(np.random.normal(np.sin(t), 0.02))
-            for t in self.train_t_vals[125:]:
-                self.training_data.append(self.training_data[-1])
+        # self.T_orig = data_period
+        # self.training_data = training_data
 
-            dt = np.max(self.train_t_vals)/(self.train_t_vals.size - 1)
-        else:
-            self.train_t_vals = training_data[0]
-            self.training_data = training_data[1]
-            dt = self.train_t_vals[1] - self.train_t_vals[0]
+        self.find_basis_functions_weights(training_data, data_period)
 
-        self.find_basis_functions_weights(self.training_data, dt)
+    def find_basis_functions_weights(self, training_data, data_period, num_weights=5):
+        """
+        Arguments:
+            data [(steps x spacial dim) np array] - data to replicate with DMP
+            data_period [float] - time duration of data
+        """
 
-    def find_basis_functions_weights(self, data, dt, num_weights=5):
+        self.training_data = training_data # for plotting
+
+        dt = data_period / len(training_data)
+
+        init_state = training_data[0]  # initial pos
+        goal_state = training_data[-1]  # assume goal is reached by end of data
 
         # means (C) and std devs (H) of gaussian basis functions
         C = np.linspace(0, 1, num_weights)
         H = (0.65*(1./(num_weights-1))**2)
 
-        q0 = data[0]  # initial pos
-        g = data[-1]  # assume goal is reached by end of data
-        self.T_orig = len(data)*dt  # time duration of data
+        for dim in range(len(training_data[0])):
 
-        q = q0
-        qd_last = 0
+            dimension_data = training_data[:,dim]
 
-        phi_vals = []
-        f_vals = []
+            q0 = init_state[dim]
+            g = goal_state[dim]
 
-        for i, _ in enumerate(data):
-            if i + 1 == len(data):
-                qd = 0
+            q = q0
+            qd_last = 0
+
+            phi_vals = []
+            f_vals = []
+
+            #TODO: fix dimensions
+            for i, _ in enumerate(dimension_data):
+                if i + 1 == len(dimension_data):
+                    qd = 0
+                else:
+                    qd = (dimension_data[i+1] - dimension_data[i]) / dt
+
+                Phi = [np.exp(-0.5 * ((i * dt / data_period) - c)**2 / H)
+                       for c in C]
+                Phi = Phi/np.sum(Phi)
+
+                qdd = (qd - qd_last)/dt
+
+                f = (qdd * data_period**2 - self.K * (g - q) + self.B * qd
+                     * data_period) / (g - q0)
+
+                phi_vals.append(Phi)
+                f_vals.append(f)
+
+                qd_last = qd
+                q += qd * dt
+
+            phi_vals = np.asarray(phi_vals)
+            f_vals = np.asarray(f_vals)
+
+            w = np.linalg.lstsq(phi_vals, f_vals, rcond=None)
+
+            if self.weights is None:
+                self.weights = np.asarray(w[0])
             else:
-                qd = (data[i+1] - data[i]) / dt
+                self.weights = np.vstack([self.weights, w[0]])
 
-            Phi = [np.exp(-0.5 * ((i * dt / self.T_orig) - c)**2 / H)
-                   for c in C]
-            Phi = Phi/np.sum(Phi)
+        return self.weights  # TODO: neccesary?
 
-            qdd = (qd - qd_last)/dt
-
-            f = (qdd * self.T_orig**2 - self.K * (g - q) + self.B * qd
-                 * self.T_orig) / (g - q0)
-
-            phi_vals.append(Phi)
-            f_vals.append(f)
-
-            qd_last = qd
-            q += qd * dt
-
-        phi_vals = np.asarray(phi_vals)
-        f_vals = np.asarray(f_vals)
-
-        w = np.linalg.lstsq(phi_vals, f_vals, rcond=None)
-        self.w = w[0]
-
-        return w[0]
-
-    def recreate_trajectory(self, q0, g, T):
-        '''
-        q0 - initial state/position
-        g  - goal state/position
+    def recreate_trajectory(self, init_state, goal_state, T):
+        """
+        init_state - initial state/position
+        goal_state - goal state/position
         T  - amount of time to travek q0 -> g
-        '''
+        """
 
-        nrBasis = len(self.w)  # number of gaussian basis functions
+        nrBasis = len(self.weights[0])  # number of gaussian basis functions
 
         # means (C) and std devs (H) of gaussian basis functions
         C = np.linspace(0, 1, nrBasis)
         H = (0.65*(1./(nrBasis-1))**2)
 
         # initialize virtual system
-        q = q0
-        qd = 0
-        qdd = 0
+        state = init_state
+        state_dot = [0,0]
+        state_double_dot = [0,0]
         t = 0
+
+        self.train_t_vals = np.arange(0, T, self.timesteps)
+        # TODO: is self.period variable used
 
         positions = []
         for k in range(self.timesteps):
+            new_state = []
             t = t + self.dt
-            if t <= T:
-                Phi = [np.exp(-0.5 * ((t / T) - c)**2 / H) for c in C]
-                Phi = Phi / np.sum(Phi)
-                f = np.dot(Phi, self.w)
-            else:
-                f = 0
 
-            # simulate dynamics
-            qdd = self.K*(g-q)/T**2 - self.B*qd/T + (g-q0)*f/T**2
-            qd = qd + qdd * self.dt
-            q = q + qd * self.dt
-            positions.append(q)
+            for dim in range(self.weights.shape[0]):
+
+                q0 = init_state[dim]
+                q = state[dim]
+                qd = state_dot[dim]
+                qdd = state_double_dot[dim]
+                g = goal_state[dim]
+
+                if t <= T:
+                    Phi = [np.exp(-0.5 * ((t / T) - c)**2 / H) for c in C]
+                    Phi = Phi / np.sum(Phi)
+                    f = np.dot(Phi, self.weights[dim])
+                else:
+                    f = 0
+
+                # simulate dynamics
+                qdd = self.K*(g-q)/T**2 - self.B*qd/T + (g-q0)*f/T**2
+                qd = qd + qdd * self.dt
+                q = q + qd * self.dt
+
+                state_dot[dim] = qd
+                state_double_dot[dim] = qdd
+
+                new_state.append(q)
+
+            state = new_state
+            positions.append(new_state)
 
         return np.arange(0, self.timesteps * self.dt, self.dt), positions
 
@@ -121,24 +159,29 @@ class DMP(object):
 
         t, pos = self.recreate_trajectory(q0, g, T)
 
+        state_hist = np.asarray(pos)
+
         if visualize:
-            plt.plot(self.train_t_vals, self.training_data,
+            plt.plot(self.training_data[:,0], self.training_data[:,1],
                      label="Training Data")
-            plt.plot(t, pos, label="DMP Approximation")
+            plt.plot(state_hist[:,0], state_hist[:,1],
+                     label="DMP Approximation")
             plt.xlabel("Time [s]")
             plt.ylabel("Position [m]")
             plt.legend()
             plt.show()
 
+
+
         return t, pos
 
     def show_DMP_purpose(self):
-        '''
+        """
         This function conveys the purpose of DMPs:
             to capture a trajectory and be able to stretch
             and squeeze it in terms of start and stop position
             or time
-        '''
+        """
         q0_orig = self.training_data[0]
         g_orig = self.training_data[-1]
 
@@ -167,6 +210,10 @@ class DMP(object):
 
 
 if __name__ == '__main__':
-    DMP_controller = DMP()
-    DMP_controller.show_DMP_purpose()
-    # DMP_controller.solve_trajectory(0, 1, 3)
+    period = np.pi / 2
+    t = np.arange(0, period, 0.01)
+    training_data = np.asarray([np.sin(t), np.cos(t)]).T
+
+    DMP_controller = DMP(training_data, period)
+    # DMP_controller.show_DMP_purpose()
+    DMP_controller.solve_trajectory([0,1], [1,0], 3)
