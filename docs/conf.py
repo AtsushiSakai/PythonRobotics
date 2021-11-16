382bf5d9cd5afa9aed2d0be2982cf2b05985acdc
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-11-16 22:59:19 +0900

    clean up doc dir

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.viewcode',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
