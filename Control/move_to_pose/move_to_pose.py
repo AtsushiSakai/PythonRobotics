a78bd2be5f3cf515742c9857653f924d5a39235c
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-12-10 15:34:18 +0100

    Added speed limitation of the robot

--- Control/move_to_pose/move_to_pose.py
+++ Control/move_to_pose/move_to_pose.py
@@ -19,6 +19,10 @@ Kp_alpha = 15
 Kp_beta = -3
 dt = 0.01
 
+# Robot specifications
+_MAX_LINEAR_SPEED = 15
+_MAX_ANGULAR_SPEED = 7
+
 show_animation = True
 
 
@@ -62,6 +66,12 @@ def move_to_pose(x_start, y_start, theta_start, x_goal, y_goal, theta_goal):
 
         if alpha > np.pi / 2 or alpha < -np.pi / 2:
             v = -v
+
+        if abs(v) > _MAX_LINEAR_SPEED:
+            v = v/abs(v) * _MAX_LINEAR_SPEED 
+
+        if abs(w) > _MAX_ANGULAR_SPEED:
+            w = w/abs(w) * _MAX_ANGULAR_SPEED
 
         theta = theta + w * dt
         x = x + v * np.cos(theta) * dt
