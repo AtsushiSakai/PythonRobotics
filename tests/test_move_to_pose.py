3e1c190dfe19e8469cb128a38dfe0142c0def7b4
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-12-12 10:46:30 +0100

    Added unit test for robot speed limitation

--- tests/test_move_to_pose.py
+++ tests/test_move_to_pose.py
@@ -6,6 +6,15 @@ def test_1():
     m.show_animation = False
     m.main()
 
+def test_2():
+    """
+    This unit test tests the move_to_pose.py program for a random 
+    MAX_LINEAR_SPEED and MAX_ANGULAR_SPEED
+    """
+    m.show_animation = False
+    m.MAX_LINEAR_SPEED = 10
+    m.MAX_ANGULAR_SPEED = 4
+    m.main()
 
 if __name__ == '__main__':
     conftest.run_this_test(__file__)
