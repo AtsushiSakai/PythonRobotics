6862fd39c2b55eb4be42b9b6b0266abb8abd0604
Atsushi Sakai <asakai.amsl+github@gmail.com>
2022-01-08 08:47:22 +0900

    enable_mpc_test

--- tests/test_model_predictive_speed_and_steer_control.py
+++ tests/test_model_predictive_speed_and_steer_control.py
@@ -1,18 +1,19 @@
 import sys
 import conftest
 
-if 'cvxpy' in sys.modules:  # pragma: no cover
+from PathTracking.model_predictive_speed_and_steer_control \
+    import model_predictive_speed_and_steer_control as m
 
-    from PathTracking.model_predictive_speed_and_steer_control \
-        import model_predictive_speed_and_steer_control as m
 
-    def test_1():
-        m.show_animation = False
-        m.main()
+def test_1():
+    m.show_animation = False
+    m.main()
 
-    def test_2():
-        m.show_animation = False
-        m.main2()
+
+def test_2():
+    m.show_animation = False
+    m.main2()
+
 
 if __name__ == '__main__':
     conftest.run_this_test(__file__)
