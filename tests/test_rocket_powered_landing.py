db69c26587e9046b4d19338976c03e304f19e442
Atsushi Sakai <asakai.amsl+github@gmail.com>
2022-01-08 10:34:11 +0900

    enable_mpc_test

--- tests/test_rocket_powered_landing.py
+++ tests/test_rocket_powered_landing.py
@@ -1,12 +1,17 @@
 import conftest  # Add root path to sys.path
 import sys
+from numpy.testing import suppress_warnings
 
 from AerialNavigation.rocket_powered_landing import rocket_powered_landing as m
 
 
 def test1():
     m.show_animation = False
-    m.main()
+    with suppress_warnings() as sup:
+        sup.filter(UserWarning,
+                   "You are solving a parameterized problem that is not DPP"
+                   )
+        m.main()
 
 
 if __name__ == '__main__':
