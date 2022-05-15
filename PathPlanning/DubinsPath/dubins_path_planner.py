561adf7e4b21a5652e564c8aafa2c833ec473a64
Atsushi Sakai <asakai.amsl+github@gmail.com>
2022-05-15 21:43:32 +0900

    Enhance dubins path docs

--- PathPlanning/DubinsPath/dubins_path_planner.py
+++ PathPlanning/DubinsPath/dubins_path_planner.py
@@ -10,7 +10,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../utils/")
 
-import math
+from math import sin, cos, atan2, sqrt, acos, pi, trunc, hypot
 import numpy as np
 from utils.angle import angle_mod, rot_mat_2d
 
@@ -95,11 +95,11 @@ def _mod2pi(theta):
 
 
 def _calc_trig_funcs(alpha, beta):
-    sin_a = math.sin(alpha)
-    sin_b = math.sin(beta)
-    cos_a = math.cos(alpha)
-    cos_b = math.cos(beta)
-    cos_ab = math.cos(alpha - beta)
+    sin_a = sin(alpha)
+    sin_b = sin(beta)
+    cos_a = cos(alpha)
+    cos_b = cos(beta)
+    cos_ab = cos(alpha - beta)
     return sin_a, sin_b, cos_a, cos_b, cos_ab
 
 
@@ -109,9 +109,9 @@ def _LSL(alpha, beta, d):
     p_squared = 2 + d ** 2 - (2 * cos_ab) + (2 * d * (sin_a - sin_b))
     if p_squared < 0:  # invalid configuration
         return None, None, None, mode
-    tmp = math.atan2((cos_b - cos_a), d + sin_a - sin_b)
+    tmp = atan2((cos_b - cos_a), d + sin_a - sin_b)
     d1 = _mod2pi(-alpha + tmp)
-    d2 = math.sqrt(p_squared)
+    d2 = sqrt(p_squared)
     d3 = _mod2pi(beta - tmp)
     return d1, d2, d3, mode
 
@@ -122,11 +122,11 @@ def _RSR(alpha, beta, d):
     p_squared = 2 + d ** 2 - (2 * cos_ab) + (2 * d * (sin_b - sin_a))
     if p_squared < 0:
         return None, None, None, mode
-    tmp = math.atan2((cos_a - cos_b), d - sin_a + sin_b)
-    t = _mod2pi(alpha - tmp)
-    p = math.sqrt(p_squared)
-    q = _mod2pi(-beta + tmp)
-    return t, p, q, mode
+    tmp = atan2((cos_a - cos_b), d - sin_a + sin_b)
+    d1 = _mod2pi(alpha - tmp)
+    d2 = sqrt(p_squared)
+    d3 = _mod2pi(-beta + tmp)
+    return d1, d2, d3, mode
 
 
 def _LSR(alpha, beta, d):
@@ -135,11 +135,11 @@ def _LSR(alpha, beta, d):
     mode = ["L", "S", "R"]
     if p_squared < 0:
         return None, None, None, mode
-    p = math.sqrt(p_squared)
-    tmp = math.atan2((-cos_a - cos_b), (d + sin_a + sin_b)) - math.atan2(-2.0, p)
-    t = _mod2pi(-alpha + tmp)
-    q = _mod2pi(-_mod2pi(beta) + tmp)
-    return t, p, q, mode
+    d1 = sqrt(p_squared)
+    tmp = atan2((-cos_a - cos_b), (d + sin_a + sin_b)) - atan2(-2.0, d1)
+    d2 = _mod2pi(-alpha + tmp)
+    d3 = _mod2pi(-_mod2pi(beta) + tmp)
+    return d2, d1, d3, mode
 
 
 def _RSL(alpha, beta, d):
@@ -148,11 +148,11 @@ def _RSL(alpha, beta, d):
     mode = ["R", "S", "L"]
     if p_squared < 0:
         return None, None, None, mode
-    p = math.sqrt(p_squared)
-    tmp = math.atan2((cos_a + cos_b), (d - sin_a - sin_b)) - math.atan2(2.0, p)
-    t = _mod2pi(alpha - tmp)
-    q = _mod2pi(beta - tmp)
-    return t, p, q, mode
+    d1 = sqrt(p_squared)
+    tmp = atan2((cos_a + cos_b), (d - sin_a - sin_b)) - atan2(2.0, d1)
+    d2 = _mod2pi(alpha - tmp)
+    d3 = _mod2pi(beta - tmp)
+    return d2, d1, d3, mode
 
 
 def _RLR(alpha, beta, d):
@@ -161,10 +161,10 @@ def _RLR(alpha, beta, d):
     tmp = (6.0 - d ** 2 + 2.0 * cos_ab + 2.0 * d * (sin_a - sin_b)) / 8.0
     if abs(tmp) > 1.0:
         return None, None, None, mode
-    p = _mod2pi(2 * math.pi - math.acos(tmp))
-    t = _mod2pi(alpha - math.atan2(cos_a - cos_b, d - sin_a + sin_b) + _mod2pi(p / 2.0))
-    q = _mod2pi(alpha - beta - t + _mod2pi(p))
-    return t, p, q, mode
+    d2 = _mod2pi(2 * pi - acos(tmp))
+    d1 = _mod2pi(alpha - atan2(cos_a - cos_b, d - sin_a + sin_b) + _mod2pi(d2 / 2.0))
+    d3 = _mod2pi(alpha - beta - d1 + _mod2pi(d2))
+    return d1, d2, d3, mode
 
 
 def _LRL(alpha, beta, d):
@@ -173,10 +173,10 @@ def _LRL(alpha, beta, d):
     tmp = (6.0 - d ** 2 + 2.0 * cos_ab + 2.0 * d * (- sin_a + sin_b)) / 8.0
     if abs(tmp) > 1.0:
         return None, None, None, mode
-    p = _mod2pi(2 * math.pi - math.acos(tmp))
-    t = _mod2pi(-alpha - math.atan2(cos_a - cos_b, d + sin_a - sin_b) + p / 2.0)
-    q = _mod2pi(_mod2pi(beta) - alpha - t + _mod2pi(p))
-    return t, p, q, mode
+    d2 = _mod2pi(2 * pi - acos(tmp))
+    d1 = _mod2pi(-alpha - atan2(cos_a - cos_b, d + sin_a - sin_b) + d2 / 2.0)
+    d3 = _mod2pi(_mod2pi(beta) - alpha - d1 + _mod2pi(d2))
+    return d1, d2, d3, mode
 
 
 _PATH_TYPE_MAP = {"LSL": _LSL, "RSR": _RSR, "LSR": _LSR, "RSL": _RSL,
@@ -187,50 +187,49 @@ def _dubins_path_planning_from_origin(end_x, end_y, end_yaw, curvature,
                                       step_size, planning_funcs):
     dx = end_x
     dy = end_y
-    d = math.hypot(dx, dy) * curvature
+    d = hypot(dx, dy) * curvature
 
-    theta = _mod2pi(math.atan2(dy, dx))
+    theta = _mod2pi(atan2(dy, dx))
     alpha = _mod2pi(-theta)
     beta = _mod2pi(end_yaw - theta)
 
     best_cost = float("inf")
-    bt, bp, bq, best_mode = None, None, None, None
+    b_d1, b_d2, b_d3, b_mode = None, None, None, None
 
     for planner in planning_funcs:
-        t, p, q, mode = planner(alpha, beta, d)
-        if t is None:
+        d1, d2, d3, mode = planner(alpha, beta, d)
+        if d1 is None:
             continue
 
-        cost = (abs(t) + abs(p) + abs(q))
+        cost = (abs(d1) + abs(d2) + abs(d3))
         if best_cost > cost:  # Select minimum length one.
-            bt, bp, bq, best_mode = t, p, q, mode
-            best_cost = cost
-    lengths = [bt, bp, bq]
+            b_d1, b_d2, b_d3, b_mode, best_cost = d1, d2, d3, mode, cost
 
+    lengths = [b_d1, b_d2, b_d3]
     x_list, y_list, yaw_list = _generate_local_course(sum(lengths), lengths,
-                                                      best_mode, curvature,
+                                                      b_mode, curvature,
                                                       step_size)
 
     lengths = [length / curvature for length in lengths]
 
-    return x_list, y_list, yaw_list, best_mode, lengths
+    return x_list, y_list, yaw_list, b_mode, lengths
 
 
 def _interpolate(ind, length, mode, max_curvature, origin_x, origin_y,
                  origin_yaw, path_x, path_y, path_yaw):
     if mode == "S":
-        path_x[ind] = origin_x + length / max_curvature * math.cos(origin_yaw)
-        path_y[ind] = origin_y + length / max_curvature * math.sin(origin_yaw)
+        path_x[ind] = origin_x + length / max_curvature * cos(origin_yaw)
+        path_y[ind] = origin_y + length / max_curvature * sin(origin_yaw)
         path_yaw[ind] = origin_yaw
     else:  # curve
-        ldx = math.sin(length) / max_curvature
+        ldx = sin(length) / max_curvature
         ldy = 0.0
         if mode == "L":  # left turn
-            ldy = (1.0 - math.cos(length)) / max_curvature
+            ldy = (1.0 - cos(length)) / max_curvature
         elif mode == "R":  # right turn
-            ldy = (1.0 - math.cos(length)) / -max_curvature
-        gdx = math.cos(-origin_yaw) * ldx + math.sin(-origin_yaw) * ldy
-        gdy = -math.sin(-origin_yaw) * ldx + math.cos(-origin_yaw) * ldy
+            ldy = (1.0 - cos(length)) / -max_curvature
+        gdx = cos(-origin_yaw) * ldx + sin(-origin_yaw) * ldy
+        gdy = -sin(-origin_yaw) * ldx + cos(-origin_yaw) * ldy
         path_x[ind] = origin_x + gdx
         path_y[ind] = origin_y + gdy
 
@@ -244,7 +243,7 @@ def _interpolate(ind, length, mode, max_curvature, origin_x, origin_y,
 
 def _generate_local_course(total_length, lengths, modes, max_curvature,
                            step_size):
-    n_point = math.trunc(total_length / step_size) + len(lengths) + 4
+    n_point = trunc(total_length / step_size) + len(lengths) + 4
 
     p_x = [0.0 for _ in range(n_point)]
     p_y = [0.0 for _ in range(n_point)]
