0705425fc8bf728fa0a7f25741467135b7c69ddc
Stephen Petrides <sdpetrides@gmail.com>
2023-02-24 19:24:21 -0500

    Add work from Hackathon on AStar and Frenet (and map files)

--- PathPlanning/AStar/a_star.py
+++ PathPlanning/AStar/a_star.py
@@ -13,6 +13,11 @@ import math
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
+from PIL import Image
+
+
 show_animation = True
 
 
@@ -234,33 +239,19 @@ def main():
     print(__file__ + " start!!")
 
     # start and goal position
-    sx = 10.0  # [m]
-    sy = 10.0  # [m]
-    gx = 50.0  # [m]
-    gy = 50.0  # [m]
-    grid_size = 2.0  # [m]
+    grid_size = 10.0  # [m]
     robot_radius = 1.0  # [m]
 
     # set obstacle positions
-    ox, oy = [], []
-    for i in range(-10, 60):
-        ox.append(i)
-        oy.append(-10.0)
-    for i in range(-10, 60):
-        ox.append(60.0)
-        oy.append(i)
-    for i in range(-10, 61):
-        ox.append(i)
-        oy.append(60.0)
-    for i in range(-10, 61):
-        ox.append(-10.0)
-        oy.append(i)
-    for i in range(-10, 40):
-        ox.append(20.0)
-        oy.append(i)
-    for i in range(0, 40):
-        ox.append(40.0)
-        oy.append(60.0 - i)
+    map = np.array(Image.open("map.pgm"))
+    map = map[400:-400, 400:-400] # crop
+    ob = np.argwhere(np.logical_or(map == 0, map == 205))
+
+    ox = ob[:, 0]
+    oy = ob[:, 1]
+
+    gx, gy = 140, 210
+    sx, sy = 120, 120
 
     if show_animation:  # pragma: no cover
         plt.plot(ox, oy, ".k")
