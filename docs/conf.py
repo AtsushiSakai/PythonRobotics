72ff23d30f00c5d63b4b5d55e14a0614d0b68e58
Faizan-Alam-1 <faizanalam2030@gmail.com>
2023-05-27 14:45:52 +0530

    Add copy button in the documentation code snippets

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.imgconverter',
     'IPython.sphinxext.ipython_console_highlighting',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
