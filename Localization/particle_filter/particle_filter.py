206ca0cb8f5b43e86beca7dc490ecdd8ab108da1
Chris7462 <chris7462@gmail.com>
2024-03-31 17:54:31 -0400

    Fix covariance dimension

--- Localization/particle_filter/particle_filter.py
+++ Localization/particle_filter/particle_filter.py
@@ -96,10 +96,10 @@ def calc_covariance(x_est, px, pw):
     calculate covariance matrix
     see ipynb doc
     """
-    cov = np.zeros((3, 3))
+    cov = np.zeros((4, 4))
     n_particle = px.shape[1]
     for i in range(n_particle):
-        dx = (px[:, i:i + 1] - x_est)[0:3]
+        dx = (px[:, i:i + 1] - x_est)
         cov += pw[0, i] * dx @ dx.T
     cov *= 1.0 / (1.0 - pw @ pw.T)
 
