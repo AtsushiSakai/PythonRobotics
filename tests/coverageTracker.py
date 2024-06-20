a48881aa2276b52e6aacf06608dea6e57b6199d4
deniseminov2017 <deniseminov2017@gmail.com>
2024-06-20 13:38:46 +0200

    Update the branch_coverage values
    Since I chose new ones I had to change the dictionary's values

--- tests/coverageTracker.py
+++ tests/coverageTracker.py
@@ -1,6 +1,6 @@
 branch_coverage = {
-    "dijkstra_1": False, # if-branch 
-    "dijkstra_2": False, #
-    "rrt_star_dubins_1": False, #
-    "rrt_star_dubins__2": False #
+    "sobol_1": False, # if branch if the value of dim_num is within the inclusive range of 1 to dim_max
+    "sobol_2": False, # if it is not
+    "pure_pursuit_1": False, # if branch if it is bigger than the accel_max
+    "pure_pursuit_2": False # if it is smaller than the accel_max
 }
