e95d0ff0fce702e5c4bbf0692dc014c594c4c1be
deniseminov2017 <deniseminov2017@gmail.com>
2024-06-27 02:59:47 +0200

    Two new print_coverage functions
    One of them is for tau_sobol and the other one is for PIDControl

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,23 +1,35 @@
 import conftest
 import numpy as np
-from coverageTracker import branch_coverage
+from coverageTracker import branch_coverage_sobol
+from coverageTracker import branch_coverage_pid
 from PathPlanning.RRT.sobol import sobol as s
 from PathPlanning.ClosedLoopRRTStar import pure_pursuit as p
 from PathPlanning.ClosedLoopRRTStar import unicycle_model as u
 
-def print_coverage():
+def print_coverage_sobol():
+    print(f"This is the branch coverage for tau_sobol: \n")
     hits = 0
-    for branch, hit in branch_coverage.items():
+    for branch, hit in branch_coverage_sobol.items():
         if hit:
             hits += 1  
-        print(f"\n{branch} was {'hit' if hit else 'not hit'}")
-    
-    if len(branch_coverage) > 0:
-        coverage_percentage = hits / len(branch_coverage) * 100
-    else:
-        coverage_percentage = 0.0
-    print(f"Total function coverage: {coverage_percentage:.2f}%")
-    
+        print(f"{branch} was {'hit' if hit else 'not hit'}")
+    percentage = 0.0
+    if len(branch_coverage_sobol) != 0:
+        percentage = hits / len(branch_coverage_sobol) * 100
+    print(f"Total function coverage: {percentage:.2f}%")
+
+def print_coverage_pid():
+    print(f"This is the branch coverage for PIDControl: \n")
+    hits = 0
+    for branch, hit in branch_coverage_pid.items():
+        if hit:
+            hits += 1  
+        print(f"{branch} was {'hit' if hit else 'not hit'}")
+    percentage = 0.0
+    if len(branch_coverage_sobol) != 0:
+        percentage = hits / len(branch_coverage_sobol) * 100
+    print(f"Total function coverage: {percentage:.2f}%")
+
 
 def test_tau_sobol_dimNum_bigger_than_dimMax():
     dimNum = 10000
@@ -35,6 +47,7 @@ def test_tau_sobol_dimNum_valid_cases():
 def test_tau_sobol_dimNum_equal_to_zero():
     dimNum = 0
     assert s.tau_sobol(dimNum) == -1
+    print_coverage_sobol()
 
 def test_PIDControl_a_bigger():
     assert p.PIDControl(100, 0) == u.accel_max
@@ -45,7 +58,7 @@ def test_PIDControl_a_smaller():
 def test_PIDControl_a_equal():
     a = p.Kp * (1 - 0)
     assert p.PIDControl(1, 0) == a
-    print_coverage()
+    print_coverage_pid()
 
 if __name__ == '__main__':
     conftest.run_this_test(__file__)
