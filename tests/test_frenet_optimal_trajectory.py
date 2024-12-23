fde87a081e69f3db94a151b7a9374a47f5af819d
Aglargil <1252223935@qq.com>
2024-12-24 00:48:09 +0800

    feat: Extend frenet_optimal_trajectory to support more scenarios

--- tests/test_frenet_optimal_trajectory.py
+++ tests/test_frenet_optimal_trajectory.py
@@ -1,5 +1,9 @@
 import conftest
 from PathPlanning.FrenetOptimalTrajectory import frenet_optimal_trajectory as m
+from PathPlanning.FrenetOptimalTrajectory.frenet_optimal_trajectory import (
+    LateralMovement,
+    LongitudinalMovement,
+)
 
 
 def test1():
@@ -8,5 +12,37 @@ def test1():
     m.main()
 
 
-if __name__ == '__main__':
+def test2():
+    m.show_animation = False
+    m.LATERAL_MOVEMENT = LateralMovement.HighSpeed
+    m.LONGITUDINAL_MOVEMENT = LongitudinalMovement.MergingAndStopping
+    m.SIM_LOOP = 5
+    m.main()
+
+
+def test3():
+    m.show_animation = False
+    m.LATERAL_MOVEMENT = LateralMovement.HighSpeed
+    m.LONGITUDINAL_MOVEMENT = LongitudinalMovement.VelocityKeeping
+    m.SIM_LOOP = 5
+    m.main()
+
+
+def test4():
+    m.show_animation = False
+    m.LATERAL_MOVEMENT = LateralMovement.HighSpeed
+    m.LONGITUDINAL_MOVEMENT = LongitudinalMovement.VelocityKeeping
+    m.SIM_LOOP = 5
+    m.main()
+
+
+def test5():
+    m.show_animation = False
+    m.LATERAL_MOVEMENT = LateralMovement.HighSpeed
+    m.LONGITUDINAL_MOVEMENT = LongitudinalMovement.MergingAndStopping
+    m.SIM_LOOP = 5
+    m.main()
+
+
+if __name__ == "__main__":
     conftest.run_this_test(__file__)
