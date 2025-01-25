6badfe8f92053207e3c1d98cf59094b1865199ff
Atsushi Sakai <asakai.amsl+github@gmail.com>
2025-01-25 21:41:42 +0900

    Update doc organization

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'PythonRobotics'
-copyright = '2018-2023, Atsushi Sakai'
+copyright = '2018-now, Atsushi Sakai'
 author = 'Atsushi Sakai'
 
 # The short X.Y version
---
+++ docs/modules/1_introduction/introduction_main.rst
@@ -0,0 +1,13 @@
+.. _introduction:
+
+Introduction
+============
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Table of Contents
+
+   1_definition_of_robotics/definition_of_robotics
+   2_software_for_robotics/software_for_robotics
+   3_technology_for_robotics/technology_for_robotics
+
