eca161e34c1466641cd4729c0073f3e2d98cec62
yangtzech <yangtzech@qq.com>
2025-04-05 22:46:42 +0800

    feat: Add reverse mode test case

--- tests/test_pure_pursuit.py
+++ tests/test_pure_pursuit.py
@@ -6,6 +6,10 @@ def test1():
     m.show_animation = False
     m.main()
 
+def test_backward():
+    m.show_animation = False
+    m.is_reverse_mode = True
+    m.main()
 
 if __name__ == '__main__':
     conftest.run_this_test(__file__)
