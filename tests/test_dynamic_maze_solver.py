62481b0af47852e31ec2b8407900a77bbd96721e
Ujjansh Sundram <ujjanshsundram2006@gmail.com>
2025-10-21 23:37:23 +0530

    update the rst and added the test file

---
+++ tests/test_dynamic_maze_solver.py
@@ -0,0 +1,26 @@
+import conftest
+from PathPlanning.BreadthFirstSearch.dynamic_maze_solver import MazeVisualizer
+
+
+def test_bfs_finds_path():
+    # small maze: 0=open, 1=wall
+    maze = [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 0, 0]
+    ]
+
+    start = (0, 0)
+    target = (2, 2)
+
+    viz = MazeVisualizer(maze, start, target)
+
+    path, visited = viz._bfs()
+
+    assert path is not None
+    assert path[0] == start
+    assert path[-1] == target
+
+
+if __name__ == '__main__':
+    conftest.run_this_test(__file__)
