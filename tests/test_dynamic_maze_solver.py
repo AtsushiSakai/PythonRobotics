ce51ed32048acf6c211486d6144683665ce9c29a
Ujjansh Sundram <ujjanshsundram2006@gmail.com>
2025-10-24 13:26:10 +0530

    updated the test file

--- tests/test_dynamic_maze_solver.py
+++ tests/test_dynamic_maze_solver.py
@@ -1,5 +1,5 @@
 import conftest
-from PathPlanning.Dynamic_maze_Solver.dynamic_maze_solver import MazeVisualizer
+from PathPlanning.DynamicMazeSolver import dynamic_maze_solver as m
 
 
 def test_bfs_finds_path():
@@ -13,13 +13,52 @@ def test_bfs_finds_path():
     start = (0, 0)
     target = (2, 2)
 
-    viz = MazeVisualizer(maze, start, target)
+    # module `dynamic_maze_solver` exposes `MazeVisualizer` class
+    viz = m.MazeVisualizer(maze, start, target)
 
     path, visited = viz._bfs()
 
     assert path is not None
     assert path[0] == start
     assert path[-1] == target
+
+
+def test_bfs_unreachable_target():
+    # target is enclosed by walls
+    maze = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+
+    start = (0, 0)
+    target = (2, 2)
+
+    viz = m.MazeVisualizer(maze, start, target)
+
+    path, visited = viz._bfs()
+
+    assert path is None
+    assert target not in visited
+
+
+def test_bfs_start_equals_target():
+    # trivial case where start == target
+    maze = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+    start = (1, 1)
+    target = start
+
+    viz = m.MazeVisualizer(maze, start, target)
+
+    path, visited = viz._bfs()
+
+    assert path is not None
+    assert path == [start]
 
 
 if __name__ == '__main__':
